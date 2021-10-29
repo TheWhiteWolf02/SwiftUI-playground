8253ff814435caa0c624f8251c7b938c6e1c5c3f
Shifat Ur Rahman <shifaturrahman@iut-dhaka.edu>
2021-10-29 22:11:57 +0200

    Testing state, binding and ObservableObject

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,16 +6,21 @@
 //
 
 import SwiftUI
+import ReactiveSwift
 
 struct ContentView: View {
+    @Binding var t: TestReactive // Updates Value, but Does not refresh the UI
+    @ObservedObject var intValue: TestReactive // Refreshes the UI for every update
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        VStack {
+            Button("Example Button", action: {
+                t.increment()
+                // t.incrementWithDelay()
+                print("inside button \(t.randomInts), \(intValue.randomInts)")
+            })
+            Text("Hello, \(t.randomInts) : Hello, \(intValue.randomInts)")
+                .padding()
+        }
     }
 }
--- Shared/First_ProjectApp.swift
+++ Shared/First_ProjectApp.swift
@@ -6,12 +6,39 @@
 //
 
 import SwiftUI
+import ReactiveSwift
 
 @main
 struct First_ProjectApp: App {
+    @State private var t: TestReactive
+    
+    init() {
+        t = TestReactive(start: 10)
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(t: $t, intValue: t)
         }
+    }
+}
+
+class TestReactive: ObservableObject {
+    @Published var randomInts: Int
+    
+    init(start: Int) {
+        randomInts = start
+    }
+    
+    func increment() {
+        self.randomInts += 1
+        print(self.randomInts)
+    }
+    
+    func incrementWithDelay() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+            self.randomInts += 1
+            print(self.randomInts)
+        })
     }
 }
